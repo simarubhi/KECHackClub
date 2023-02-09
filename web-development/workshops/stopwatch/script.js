7ef06260652b2ae353271c4e8dc6d3d20f9409c3
Simar Ubhi <73083235+simarubhi@users.noreply.github.com>
2023-02-09 07:58:37 -0600

    git commit -m "JS comments added"

--- web-development/workshops/stopwatch/script.js
+++ web-development/workshops/stopwatch/script.js
@@ -1,63 +1,71 @@
-const stopWatch = document.querySelector('.stopwatch');
+const stopWatch = document.querySelector('.stopwatch'); // Get the stopwatch element
 
-const startBtn = stopWatch.querySelector('.btn.start');
-const stopBtn = stopWatch.querySelector('.btn.stop');
-const resetBtn = stopWatch.querySelector('.btn.reset');
+const startBtn = stopWatch.querySelector('.btn.start'); // From the stopwatch get the start button
+const stopBtn = stopWatch.querySelector('.btn.stop'); // From the stopwatch get the stop button
+const resetBtn = stopWatch.querySelector('.btn.reset'); // From the stopwatch get the reset button
 
-const hourDisplay = stopWatch.querySelector('.time-value.hour');
-const minuteDisplay = stopWatch.querySelector('.time-value.minute');
-const secondDisplay = stopWatch.querySelector('.time-value.second');
+const hourDisplay = stopWatch.querySelector('.time-value.hour'); // From the stopwatch get the hour display element
+const minuteDisplay = stopWatch.querySelector('.time-value.minute'); // From the stopwatch get the minute display element
+const secondDisplay = stopWatch.querySelector('.time-value.second'); // From the stopwatch get the second display element
 
-let hourValue = 0;
-let minuteValue = 0;
-let secondValue = 0;
-let timerCounting = false;
+let hourValue = 0; // This will hold our hour value
+let minuteValue = 0; // This will hold our minute value
+let secondValue = 0; // This will hold our second value
+let timerCounting = false; // This will track if our timer is counting
 
+// When the start button is clicked
 startBtn.addEventListener('click', () => {
-	timerCounting = true;
-	startBtn.innerHTML = 'Start';
-	timerCount();
+	timerCounting = true; // Set the timer counting to true
+	startBtn.innerHTML = 'Start'; // Change the start button text to Start (Because it might be at resume if stopped was pressed before)
+	timerCount(); // Start counting
 });
 
+// When the stop button is clicked
 stopBtn.addEventListener('click', () => {
-	timerCounting = false;
-	startBtn.innerHTML = 'Resume';
+	timerCounting = false; // Set the timer counting to false
+	startBtn.innerHTML = 'Resume'; // Change the start button text to resume
 });
 
+// When the reset button is clicked
 resetBtn.addEventListener('click', () => {
-	timerCounting = false;
-	startBtn.innerHTML = 'Start';
-	hourValue = 0;
-	minuteValue = 0;
-	secondValue = 0;
-	updateDisplay();
+	timerCounting = false; // Set the timer counting to false
+	startBtn.innerHTML = 'Start'; // Change the start button text to Start (Because it might be at resume if stopped was pressed before)
+	hourValue = 0; // Set the hour value to 0
+	minuteValue = 0; // Set the minute value to 0
+	secondValue = 0; // Set the second value to 0
+	updateDisplay(); // Change the display to show all 0s
 });
 
+// This function counts our time
 const timerCount = () => {
-	if (!timerCounting) return;
+	if (!timerCounting) return; // If the timer is not counting then don't proceed
 
-	secondValue++;
+	secondValue++; // Increase the second value by one second
 
+	// If the second value has reached one minute
 	if (secondValue == 60) {
-		minuteValue++;
-		secondValue = 0;
+		minuteValue++; // Increase the minute by 1
+		secondValue = 0; // Reset the seconds back to 0
 	}
 
+	// If the minute value has reach one hour
 	if (minuteValue == 60) {
-		hourValue++;
-		minuteValue = 0;
+		hourValue++; // Increase the hour by 1
+		minuteValue = 0; // Reset the minute to 0
 	}
 
-	updateDisplay();
+	updateDisplay(); // Show the new time values
 
-	setTimeout(timerCount, 1000);
+	setTimeout(timerCount, 1000); // Count ever 1000ms (1 second)
 };
 
+// Show the new time values
 const updateDisplay = () => {
+	// If the seconds is less than 10
 	if (hourValue < 10) {
-		hourDisplay.innerHTML = '0' + hourValue;
+		hourDisplay.innerHTML = '0' + hourValue; // Add a 0 (as a string) in front of the value so the clock keeps the same display format
 	} else {
-		hourDisplay.innerHTML = hourValue;
+		hourDisplay.innerHTML = hourValue; // Otherwise display the two digit value
 	}
 
 	if (minuteValue < 10) {
