1bc869e4fce34212bec435ca8132fa5a697637c9
Simar Ubhi <73083235+simarubhi@users.noreply.github.com>
2023-03-02 23:08:32 -0600

    Create script.js

---
+++ web-development/workshops/stopwatch/2023-03-09/script.js
@@ -0,0 +1,12 @@
+// Bring in elements
+const stopWatch = document.querySelector('.stopwatch');
+
+// Bring in buttons
+const startBtn = stopWatch.querySelector('.btn.start');
+const stopBtn = stopWatch.querySelector('.btn.stop');
+const resetBtn = stopWatch.querySelector('.btn.reset');
+
+// Bring in display numbers
+const hourDisplay = stopWatch.querySelector('.time-value.hour');
+const minuteDisplay = stopWatch.querySelector('.time-value.minute');
+const secondDisplay = stopWatch.querySelector('.time-value.second');
